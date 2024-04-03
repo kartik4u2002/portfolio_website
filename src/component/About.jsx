32f929f710d34688dcbe84b655514685cc39cc4a
kartik4u2002 <kartik4u2002@gmail.com>
2024-04-03 20:58:23 +0530

    Just Added the image to the about section

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function About() {
   return (
     <div className="w-screen h-[30vw] bg-slate-900 flex gap-10 mt-5 justify-center items-center rounded-xl overflow-hidden">
-      <div className="w-1/4 bg-slate-100 h-[20vw] rounded-lg ml-5"></div>
+      <div className="w-1/4 bg-zinc-50 h-[20vw] rounded-xl ml-5 flex justify-center items-center overflow-hidden p-5">
+        <img src="pic.jpg" alt="profile pic" className=" rounded-full relative w-70 border-solid border-2 border-black" />
+      </div>
       <div className="w-3/4 h-[20vw] bg-slate-500 rounded-lg flex justify-center items-center mr-5">
         <p className="p-10">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit a
--- src/component/Download.jsx
+++ src/component/Download.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 function Download() {
+
+  const handleDownload = () => {
+    // Replace 'your_resume_file_path' with the actual path of your resume file
+    const resumeFilePath = 'new_resume.pdf';
+    // Triggering download
+    const link = document.createElement('a');
+    link.href = resumeFilePath;
+    link.download = 'new_resume.pdf'; // You can customize the downloaded file name here
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
-    <div className='bg-white w-full flex justify-center items-center rounded-b-xl pb-10' >
-        <div className='bg-slate-600 w-1/2 h-[10vw] rounded-xl flex justify-center items-center hover:cursor-pointer hover:bg-slate-400'>
-            <h2 className='uppercase text-2xl hover:text-black'>Download Resume</h2>
-        </div>
+    <div className='bg-white w-full flex justify-center items-center rounded-b-xl pb-10'>
+      <div 
+        className='bg-slate-600 w-1/2 h-[10vw] rounded-xl flex justify-center items-center hover:cursor-pointer hover:bg-slate-400'
+        onClick={handleDownload}
+      >
+        <h2 className='uppercase text-2xl hover:text-black'>Download Resume</h2>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Download
+export default Download;
