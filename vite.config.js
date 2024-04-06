75c5726089751f144d0181184aaed1f3adf64758
kartik4u2002 <kartik4u2002@gmail.com>
2024-04-06 14:58:52 +0530

    Deploying

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import { VitePluginGhPages } from 'vite-plugin-gh-pages';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    VitePWA(),
+    VitePluginGhPages({
+      branch: 'master', // Specify the branch to deploy to
+      dir: 'kartik4u2002/postfolio_website', // Specify the directory to deploy
+    }),
+  ],
+});
